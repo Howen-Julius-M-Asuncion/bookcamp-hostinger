bd243740309d6eb3edf43f1513d2150425514af0
John Michael Goco <jmgoco416@gmail.com>
2025-02-03 20:53:32 +0800

    Added necessary style modules

---
+++ modules/scripts.php
@@ -0,0 +1,9 @@
+<!-- BS5 -->
+<script src="<?php echo BASE_URL?>/modules/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo BASE_URL?>/modules/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+
+<!-- Modules -->
+<script src="<?php echo BASE_URL?>/modules/DataTables/datatables.min.js"></script>
+<script src="<?php echo BASE_URL?>/modules/multiselect/js/jquery.multi-select.js"></script>
+<script src="<?php echo BASE_URL?>/modules/select2/js/select2.full.min.js"></script>
+<script src="<?php echo BASE_URL?>/modules/select2/js/select2.min.js"></script>
---
+++ modules/styling.php
@@ -0,0 +1,18 @@
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://kit.fontawesome.com/71f85e3db5.js" crossorigin="anonymous"></script> -->
+
+
+<!-- BS5 -->
+<link rel="stylesheet" href="<?php echo BASE_URL?>/modules/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+
+<!-- FontAwesome (ICONS) -->
+<link href="<?php echo BASE_URL?>/modules/fontawesome-free/css/all.css" rel="stylesheet" />
+<link href="<?php echo BASE_URL?>/modules/fontawesome-free/css/v4-font-face.css"rel="stylesheet" />
+<link href="<?php echo BASE_URL?>/modules/fontawesome-free/css/v4-shims.css"rel="stylesheet" />
+
+<!-- Plugins -->
+<link rel="stylesheet" href="<?php echo BASE_URL?>/modules/DataTables/datatables.min.css">
+<link rel="stylesheet" href="<?php echo BASE_URL?>/modules/multiselect/css/multi-select.css">
+<link rel="stylesheet" href="<?php echo BASE_URL?>/modules/select2/css/select2.min.css">
+<link rel="stylesheet" href="<?php echo BASE_URL?>/modules/select2/css/select2-bootstrap-5-theme.min.css">
