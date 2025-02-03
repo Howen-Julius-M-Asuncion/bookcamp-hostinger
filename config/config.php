17ca3b2225af36bc4583871ae0e8023b0555f00f
John Michael Goco <jmgoco416@gmail.com>
2025-02-03 20:59:27 +0800

    Added Config Files

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+// Website configuration
+define('SITE_NAME', 'CampBook');
+define('BASE_URL', 'http://localhost/webdev2');
+define('FAVICON', BASE_URL . '/public/favicon.ico');
+
+// Enable authentication and admin session variables
+$debugModeAuth = false;
+$debugModeAdmin = true;
+$debugModeUser = 0;
+?>
